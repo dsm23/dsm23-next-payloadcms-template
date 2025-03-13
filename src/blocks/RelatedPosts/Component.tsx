bdf3fb6c0aca89d77a98a1ed3ee3b327a14bcc6b
David Murdoch <dsmurdoch357@gmail.com>
2025-03-13 19:08:35 +0000

    chore(ts types): add  verbatimModuleSyntax

--- src/blocks/RelatedPosts/Component.tsx
+++ src/blocks/RelatedPosts/Component.tsx
@@ -5,7 +5,7 @@ import RichText from "~/components/RichText";
 import type { Post } from "~/payload-types";
 
 import { Card } from "../../components/Card";
-import { SerializedEditorState } from "@payloadcms/richtext-lexical/lexical";
+import type { SerializedEditorState } from "@payloadcms/richtext-lexical/lexical";
 
 export type RelatedPostsProps = {
   className?: string;
