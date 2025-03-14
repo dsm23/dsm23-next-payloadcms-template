8241cb526f579fdd9be2273dfcb8f2411ef1faeb
David Murdoch <dsmurdoch357@gmail.com>
2025-03-14 20:18:11 +0000

    chore(next config): change next config to ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
+import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 import redirects from "./redirects.js";
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+  : process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
@@ -14,7 +14,7 @@ const nextConfig = {
 
         return {
           hostname: url.hostname,
-          protocol: url.protocol.replace(":", ""),
+          protocol: url.protocol.replace(":", "") as "http" | "https",
         };
       }),
     ],
