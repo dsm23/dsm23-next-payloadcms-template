935c15c3ed539bc12fe0087c709463352baf47c1
David Murdoch <dsmurdoch357@gmail.com>
2025-04-07 15:39:03 +0100

    chore(mailpit): add mailpit service in docker

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,5 +5,11 @@ declare namespace NodeJS {
     readonly MINIO_PORT: number;
     readonly MINIO_ACCESS_KEY: string;
     readonly MINIO_SECRET_KEY: string;
+    readonly SMTP_ADMIN_EMAIL: string;
+    readonly SMTP_HOST: string;
+    readonly SMTP_PORT: string;
+    readonly SMTP_USER: string;
+    readonly SMTP_PASS: string;
+    readonly SMTP_SENDER_NAME: string;
   }
 }
