f2c09e25298d7d59eb50e488bf06faa2a3798be0
David Murdoch <dsmurdoch357@gmail.com>
2025-04-25 17:48:48 +0100

    ci(docker): add docker build check pipeline

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,16 +1,16 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly ANALYZE: string;
-    readonly MINIO_BUCKET: string;
-    readonly MINIO_ENDPOINT: string;
-    readonly MINIO_PORT: number;
-    readonly MINIO_ACCESS_KEY: string;
-    readonly MINIO_SECRET_KEY: string;
-    readonly SMTP_ADMIN_EMAIL: string;
-    readonly SMTP_HOST: string;
-    readonly SMTP_PORT: string;
-    readonly SMTP_USER: string;
-    readonly SMTP_PASS: string;
-    readonly SMTP_SENDER_NAME: string;
+    readonly ANALYZE?: string;
+    readonly MINIO_BUCKET?: string;
+    readonly MINIO_ENDPOINT?: string;
+    readonly MINIO_PORT?: number;
+    readonly MINIO_ACCESS_KEY?: string;
+    readonly MINIO_SECRET_KEY?: string;
+    readonly SMTP_ADMIN_EMAIL?: string;
+    readonly SMTP_HOST?: string;
+    readonly SMTP_PORT?: string;
+    readonly SMTP_USER?: string;
+    readonly SMTP_PASS?: string;
+    readonly SMTP_SENDER_NAME?: string;
   }
 }
