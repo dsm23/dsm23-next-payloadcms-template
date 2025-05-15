4f9d10f39ab93582a5bbaee4139c467d8b7f8f5c
David Murdoch <dsmurdoch357@gmail.com>
2025-05-15 01:56:25 +0100

    fix(search): categories search sync
    * https://github.com/payloadcms/payload/pull/12359
    * Previously, search sync with categories didn't work and additionally
    caused problems with Postgres. Additionally, ensures that when doing
    synchronization, all the categories are populated, since we don't always
    have populated data inside hooks.

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -795,8 +795,9 @@ export interface Search {
   categories?:
     | {
         relationTo?: string | null;
-        id?: string | null;
+        categoryID?: string | null;
         title?: string | null;
+        id?: string | null;
       }[]
     | null;
   updatedAt: string;
@@ -1465,8 +1466,9 @@ export interface SearchSelect<T extends boolean = true> {
     | T
     | {
         relationTo?: T;
-        id?: T;
+        categoryID?: T;
         title?: T;
+        id?: T;
       };
   updatedAt?: T;
   createdAt?: T;
