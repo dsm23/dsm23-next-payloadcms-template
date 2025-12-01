6ea709a769b8655d7b496fa8db116ad7d422217b
David Murdoch <dsmurdoch357@gmail.com>
2025-12-01 13:57:10 +0000

    chore(deps): update dependency prettier to v3.7.1

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -36,8 +36,7 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-  extends ComponentProps<"button">,
-    VariantProps<typeof buttonVariants> {
+  extends ComponentProps<"button">, VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
